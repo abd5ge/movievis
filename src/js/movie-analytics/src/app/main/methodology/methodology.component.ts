bb1fbb337084a2ba8609eb17e0b55d96d720e004
platanobailando <cjmang@gmail.com>
2020-04-15 19:25:17 -0500

    Updating Approach page.
    Adding more content describing strengths and limitations
    of our approach.  Switching package-lock.json to
    npm-shrinkwrap.json for repeatability purposes.

--- src/js/movie-analytics/src/app/main/methodology/methodology.component.ts
+++ src/js/movie-analytics/src/app/main/methodology/methodology.component.ts
@@ -1,15 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-methodology',
   templateUrl: './methodology.component.html',
   styleUrls: ['./methodology.component.css']
 })
-export class MethodologyComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class MethodologyComponent { }
